79020bc6f20de9f662dd79fec7f6f5c426e9f5f5
Daniel Marshall <dmarshall90@gatech.edu>
2022-03-28 19:38:07 -0400

    working twosum

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using LeetCode.Easy;
+
+var twoSum = new TwoSum();
+twoSum.NumberArray = new int[] { 0, 4, 3, 0 };
+Console.WriteLine($"[{string.Join(", ", twoSum.GetIndicesThatAddToTarget(0))}]");
