e92b2d418921267c7956aed607bb7f54a6348022
dmarsh12 <danmarsh6@gmail.com>
2023-07-25 16:46:50 -0400

    Added myqueue - should be stack but whatever

---
+++ LeetCode/Easy/MyQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class MyQueue
+    {
+        private List<int> queue = new List<int>();
+
+        public void Push(int x)
+        {
+            queue.Add(x);
+        }
+
+        public int Pop()
+        {
+            var result = queue[0];
+            queue.Remove(result);
+            return result;
+        }
+
+        public int Peek()
+        {
+            return queue[0];
+        }
+
+        public bool Empty()
+        {
+            return queue.Any();
+        }
+    }
+}
