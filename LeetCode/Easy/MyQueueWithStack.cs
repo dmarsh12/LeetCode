4b5f88258f89f4cc4f3a59d364437ff439adba59
dmarsh12 <danmarsh6@gmail.com>
2023-07-29 12:17:15 -0400

    Added myqueuewithstack

---
+++ LeetCode/Easy/MyQueueWithStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace LeetCode.Easy
+{
+	public class MyQueueWithStack
+	{
+
+        private Stack<int> StackOne = new Stack<int>();
+        private Stack<int> StackTwo = new Stack<int>();
+
+        public void Push(int x)
+        {
+            StackOne.Push(x);
+        }
+
+        public int Pop()
+        {
+            while (StackOne.Count > 0)
+                StackTwo.Push(StackOne.Pop());
+
+            var result = StackTwo.Pop();
+
+            while (StackTwo.Count > 0)
+                StackOne.Push(StackTwo.Pop());
+
+            return result;
+        }
+
+        public int Peek()
+        {
+            while (StackOne.Count > 0)
+                StackTwo.Push(StackOne.Pop());
+
+            var result = StackTwo.Peek();
+
+            while (StackTwo.Count > 0)
+                StackOne.Push(StackTwo.Pop());
+
+            return result;
+        }
+
+        public bool Empty()
+        {
+            return StackOne.Count == 0;
+        }
+    }
+}
+
