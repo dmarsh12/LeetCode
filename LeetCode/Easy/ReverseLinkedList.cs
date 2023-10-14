7ecac22c47eefee3bf415190b736237060e05b7b
dmarsh12 <danmarsh6@gmail.com>
2023-10-13 22:30:05 -0400

    Reverse a linked list

---
+++ LeetCode/Easy/ReverseLinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using LeetCode.Models;
+
+namespace LeetCode.Easy
+{
+	public static class ReverseLinkedList
+	{
+
+		public static ListNode ExampleOne()
+		{
+			var head = new ListNode()
+			{
+				val = 5,
+				next = new ListNode()
+				{
+					val = 4,
+					next = new ListNode()
+					{
+						val = 3
+					}
+				}
+			};
+
+			return Reverse(head);
+		}
+
+		public static ListNode Reverse(ListNode head)
+		{
+			ListNode result = null;
+			while (head != null)
+			{
+				result = new ListNode(head.val, result);
+				head = head.next;
+			}
+
+			return result;
+		}
+	}
+}
+
