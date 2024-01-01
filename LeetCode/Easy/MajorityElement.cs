2b3d366f3a7d63f9a115797e85a8e8fdb746d6f3
dmarsh12 <danmarsh6@gmail.com>
2024-01-01 17:11:10 -0500

    added files

---
+++ LeetCode/Easy/MajorityElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public static class MajorityElement
+    {
+        public static int Get(int[] nums)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length / 2]; // because we know that there is always a majority, we can safely just take the middle after the sort.
+        }
+    }
+}
--- LeetCode/Easy/ReverseLinkedList.cs
+++ LeetCode/Easy/ReverseLinkedList.cs
@@ -39,9 +39,25 @@ namespace LeetCode.Easy
 
             while (current != null)
             {
-                next = current.next; // the next node in the list
+                next = current.next; // the next node in the list.. we need a next variable because we modify current.next in the next line...
                 current.next = previous; // the next value is the previous one, starts at null. since we always set next to previous, we only really need to make sure we get the head right which is setting the previous = current...
                 previous = current; // the previously visited node is the current one which starts at the head
+                current = next;
+            }
+
+            return previous;
+        }
+
+        public static ListNode ReverseItAgain(ListNode head)
+        {
+            ListNode previous = null;
+            ListNode current = head;
+
+            while (current != null)
+            {
+                var next = current.next;
+                current.next = previous;
+                previous = current;
                 current = next;
             }
 
