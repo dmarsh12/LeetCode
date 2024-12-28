8d88ea1436038a8db73d72675cc90365e22bf8d7
dmarsh12 <danmarsh6@gmail.com>
2024-12-28 00:12:48 -0500

    Test

---
+++ LeetCode/Medium/RemoveDuplicatesMedium.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace LeetCode.Medium
+{
+    public static class RemoveDuplicatesMedium
+    {
+        // Allow only one duplicate
+        public static int Execute(int[] nums)
+        {
+            int count = 0;
+            var lookup = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (lookup.ContainsKey(num)) 
+                {
+                    if (lookup[num] < 2)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        for (int j = i; j <= nums.Length-1; j++)
+                        {
+                            if (j < nums.Length-1)
+                                nums[j] = nums[j+1];
+                        }                                      
+                    }
+
+                    lookup[num]++;
+                }
+                else
+                {
+                    lookup[num] = 1;
+                }
+            }
+
+            return count;
+        }        
+    }
+}
