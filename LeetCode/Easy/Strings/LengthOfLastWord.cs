2d02613a12784ccb361254af7355637736add17e
dmarsh12 <danmarsh6@gmail.com>
2025-01-01 22:22:27 -0500

    More coding exercises

---
+++ LeetCode/Easy/Strings/LengthOfLastWord.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode.Easy.Strings;
+
+public static class LengthOfLastWord
+{
+    public static int Execute(string sentence) => sentence.Split(" ").Where(w => !string.IsNullOrEmpty(w)).Last().Length;
+}
